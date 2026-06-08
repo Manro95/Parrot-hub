6de290b623b41252d541a683662505e59b90d6ab
Roman Vranekovic <roman.vranekovic@gmail.com>
2026-06-08 22:14:00 +0200

    Update parrot data v1

---
+++ src/data/parrots.js
@@ -0,0 +1,21 @@
+const Parrots = [ {
+    id: 1,
+    name: "White Bellied Caique",
+    image: "images/White-Bellied-Caique.jpg",
+    diet: ["Fruits", "Vegetables", "Seeds", "Nuts", "Pellets"],
+    toys: ["Chew Toys", "Foraging Toys", "Climbing Toys"],
+    avoid: ["Avocado", "Chocolate", "Caffeine", "Alcohol", "Onions", "Garlic"],
+    habitat: "Native to the Amazon Basin in South America, they thrive in tropical rainforests and are often found in the canopy layer.",
+    description: "The White Bellied Caique is a small, colorful parrot known for its playful and energetic personality. They have a distinctive white belly and vibrant green, yellow, and orange feathers. These parrots are highly social and enjoy interacting with their owners, making them great pets for those who can provide them with plenty of attention and mental stimulation."
+  },
+  {
+    id: 2,
+    name: "African Grey Parrot",
+    image: "images/African/Gray.jpg",
+    diet: ["Fruits", "Vegetables", "Seeds", "Nuts", "Pellets"],
+    toys: ["Puzzle Toys", "Foraging Toys", "Climbing Toys"],
+    avoid: ["Avocado", "Chocolate", "Caffeine", "Alcohol", "Onions", "Garlic"],
+    habitat: "Native to the rainforests of West and Central Africa, they are often found in dense forests and savannas.",
+    description: "The African Grey Parrot is renowned for its exceptional intelligence and ability to mimic human speech. They have a striking grey plumage with a bright red tail. These parrots are highly social and require a lot of mental stimulation, making them ideal companions for experienced bird owners who can provide them with the attention they need."
+  }
+]
